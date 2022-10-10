090e5a5d427e407cbe5d9f248ea69ca5e2b3102f
AMESocker <aesocher@yahoo.com>
2022-10-09 18:10:44 -0700

    fixed button not linking to next page

--- contents/script.js
+++ contents/script.js
@@ -1,16 +1,25 @@
-//var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-// api Key - 6e276c3617ea3475d24d053e638998d7
-var forecastButton = document.getElementById("getForecastButton")
-var locationBox = document.getElementById("Location")
-// document.getElementById("getForecastButton").addEventListener("click",console.log('Why cant they call me the Adorable Snowman'));
-// var resultsPage = ;
-function nextPage (){
-    localStorage.setItem("LocationLS", locationBox.val());
-    console.log('Do I look abominable to you?');
-    console.log();
+var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+var apiKey = "6e276c3617ea3475d24d053e638998d7";
+var locationBox = $("#Location").val();
+var forecastButton = document.getElementById("getForecastButton");
+
+forecastButton.addEventListener("click",nextPage);
+
+function nextPage (){                                   //puts input element into Local Storage
+    document.location = ("./pages/Locations.html");
+    localStorage.setItem("LocationLS", $("#Location").val());
+    localStorage.setItem("LocationL1", $("#Location").val());
+    
+    console.log("Hello")
 };
+
+
+
+console.log($("#Location").val());
+
+// console.log(nextPage)
 // forecastButton.onclick = nextPage
-console.log(nextPage)
+// console.log(nextPage)
 
 
 // forecastButton.addEventListener("click", console.log('Why can’t they call me the Adorable Snowman'));
