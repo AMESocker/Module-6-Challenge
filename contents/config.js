375bedce3e4838c4dd5566e1b58be9a6d954ba77
AMESocker <aesocher@yahoo.com>
2022-10-11 20:29:43 -0700

    added config.js for key

---
+++ contents/config.js
@@ -0,0 +1,8 @@
+var config = {
+ 
+    MY_KEY : '123456',   
+    // SECRET_KEY : '6e276c3617ea3475d24d053e638998d7',
+    SECRET_KEY : 'b990dafb51300bdcc0b9870093afbd85',
+    KEY_2 : '101010'
+    
+   }
